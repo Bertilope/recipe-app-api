d99b4eaf22f1d29711cf964928e8965053635dbb
Bertilope <injubert@gmail.com>
2026-01-06 18:16:46 +0100

    Refactor wait_for_db command and update test descriptions for clarity

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -10,11 +10,11 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-    """Tests for custom django management commands."""
+    """Test commands."""
 
-    
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for db when db is available."""
         patched_check.return_value = True
